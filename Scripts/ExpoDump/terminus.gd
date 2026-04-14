542894b70214893e719b46f8322b3c124e180714
Chromalescence Labs <chromalescencelabs@gmail.com>
2026-04-14 20:35:07 +0800

    Delete Scripts directory

--- Scripts/ExpoDump/terminus.gd
+++
@@ -1,9 +0,0 @@
-extends Sprite2D
-
-func _ready() -> void:
-	self.modulate.a = 0
-	
-func beVisible():
-	var tween = create_tween().tween_property(self, "modulate:a", 1, 1).set_ease(Tween.EASE_IN_OUT)
-	
-	return tween
--- Scripts/Level Generation/arrow.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-var rotations = [0, 90, -180, -90]
-var rot = randi_range(0, 3)
-
-func _ready() -> void:
-	self.rotation_degrees = rotations[rot]
-	
